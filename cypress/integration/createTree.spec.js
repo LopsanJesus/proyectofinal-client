5a4208f5c81af1a2f2f3de1766ebad299772e5e7
LopsanJesus <55506008+LopsanJesus@users.noreply.github.com>
2020-12-15 00:29:27 +0100

    Comment 2 integration test

--- cypress/integration/createTree.spec.js
+++ cypress/integration/createTree.spec.js
@@ -1,21 +1,21 @@
-var faker = require('faker');
+// var faker = require('faker');
 
-context("Create Tree", () => {
-    it("can create a tree", () => {
-        cy.autoLogin();
+// context("Create Tree", () => {
+//     it("can create a tree", () => {
+//         cy.autoLogin();
 
-        cy.get(".createTreeButton").click();
+//         cy.get(".createTreeButton").click();
 
-        cy.location("pathname").should("include", "create-tree");
+//         cy.location("pathname").should("include", "create-tree");
 
-        const fakeName = "Test " + faker.lorem.word(10);
+//         const fakeName = "Test " + faker.lorem.word(10);
 
-        cy.get("#formBasicName").type(fakeName);
-        cy.get("#formBasicSourceLang").select("Inglés");
-        cy.get("#formBasicTargetLang").select("Español");
-        cy.get("button[type='submit']").click();
+//         cy.get("#formBasicName").type(fakeName);
+//         cy.get("#formBasicSourceLang").select("Inglés");
+//         cy.get("#formBasicTargetLang").select("Español");
+//         cy.get("button[type='submit']").click();
 
-        cy.location("pathname").should("include", "my-forest");
-        cy.get('.card-title .title').last().should("have.text", fakeName);
-    });
-});
+//         cy.location("pathname").should("include", "my-forest");
+//         cy.get('.card-title .title').last().should("have.text", fakeName);
+//     });
+// });

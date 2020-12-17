0c5a6ac6f90175290929e40276933ab11f42d059
Jesús López Sánchez <lopsanjesus@gmail.com>
2020-12-17 04:10:28 +0100

    Fix createBranch test and create Waits for Graphql

--- cypress/integration/createBranch.spec.js
+++ cypress/integration/createBranch.spec.js
@@ -4,7 +4,7 @@ context("Create Branch", () => {
     it("can create a branch", () => {
         cy.autoLogin();
         cy.wait(1000);
-        cy.visit("tree/4");
+        cy.visit("tree/10");
 
         cy.get(".createTreeButton").click();
 
@@ -19,13 +19,13 @@ context("Create Branch", () => {
         cy.get("button div").click();
         cy.wait(1000);
 
-        cy.location("pathname").should("include", "tree/4");
+        cy.location("pathname").should("include", "tree/10");
     });
 
     it("can't create with empty branch name", () => {
         cy.autoLogin();
         cy.wait(1000);
-        cy.visit("tree/4");
+        cy.visit("tree/10");
 
         // cy.location("pathname").should("include", "/tree/4");
 
--- cypress/integration/window.spec.js
+++ cypress/integration/window.spec.js
@@ -3,4 +3,20 @@ context("Window", () => {
         cy.visit("/");
         cy.title().should('include', 'Treelang')
     });
+
+    it("waits for GET", () => {
+        cy.intercept('POST', '**/graphql').as('getGraphql')
+
+        cy.visit("/");
+        cy.title().should('include', 'Treelang')
+
+
+        // we have code that gets a comment when
+        // the button is clicked in scripts.js
+        //cy.get('.network-btn').click()
+
+
+        // wait for GET comments/1
+        cy.wait('@getGraphql').its('response.statusCode').should('be.oneOf', [200, 304])
+    });
 });

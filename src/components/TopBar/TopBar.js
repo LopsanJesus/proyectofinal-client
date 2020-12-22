17778106d96a0132df92534ea26c45620176d4af
Paco Lopez <francisco.lopsan@gmail.com>
2020-12-22 18:02:13 +0100

    Fix double link problem on NavDropdown.Item
    use "as" property to render them directly as Link components

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -8,59 +8,64 @@ import { NavDropdown } from "react-bootstrap";
 import "./TopBar.scss";
 import { useTranslation } from "react-i18next";
 
-
-const TopBar = ({
-  user
-}) => {
+const TopBar = ({ user }) => {
   const { t } = useTranslation();
   const src = "../../../full-apple.png";
 
   return (
-    <Navbar bg="light" variant="light" expand="sm" sticky="top">
+    <Navbar bg="light" variant="light" expand="md" sticky="top">
       <Navbar.Brand>
-        <Link to="/"><img src="/treelang-title.png" alt={appConfig.title} /></Link>
+        <Link to="/">
+          <img src="/treelang-title.png" alt={appConfig.title} />
+        </Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Item className="mr-auto">
-            <Link to="/discover">{t('discover.link')}</Link>
+            <Link to="/discover">{t("discover.link")}</Link>
           </Nav.Item>
-          {user && <>
-            <Nav.Item className="mr-auto">
-              <Link to="/my-forest">{t('myForest')}</Link>
-            </Nav.Item>
-            <Nav.Item className="mr-auto">
-              <Link to="/my-history">{t('myHistory')}</Link>
-            </Nav.Item>
-          </>}
+          {user && (
+            <>
+              <Nav.Item className="mr-auto">
+                <Link to="/my-forest">{t("myForest")}</Link>
+              </Nav.Item>
+              <Nav.Item className="mr-auto">
+                <Link to="/my-history">{t("myHistory")}</Link>
+              </Nav.Item>
+            </>
+          )}
         </Nav>
         <Nav id="right-nav">
-          {user ? <>
-            <Navbar.Text className="Username">{user && user.name}</Navbar.Text>
-            <NavDropdown
-              title={
-                <span>
-                  <img className="profile-image" src={src} alt="User Pic" />
-                </span>
-              }
-              id="collasible-nav-dropdown"
-            >
-              <NavDropdown.Item className="mr-auto">
-                <Link to="/profile">{t('profile')}</Link>
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item className="mr-auto">
-                <Link to="/logout">{t('logout')}</Link>
-              </NavDropdown.Item>
-            </NavDropdown>
-          </>
-            :
+          {user ? (
+            <>
+              <Navbar.Text className="Username">
+                {user && user.name}
+              </Navbar.Text>
+              <NavDropdown
+                title={
+                  <span>
+                    <img className="profile-image" src={src} alt="User Pic" />
+                  </span>
+                }
+                id="collasible-nav-dropdown"
+              >
+                <NavDropdown.Item as={Link} to="/profile" className="mr-auto">
+                  {t("profile")}
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item as={Link} to="/logout" className="mr-auto">
+                  {t("logout")}
+                </NavDropdown.Item>
+              </NavDropdown>
+            </>
+          ) : (
             <>
               <Nav.Link className="mr-auto">
-                <Link to="/login">{t('form.login')}</Link>
+                <Link to="/login">{t("form.login")}</Link>
               </Nav.Link>
-            </>}
+            </>
+          )}
         </Nav>
       </Navbar.Collapse>
     </Navbar>

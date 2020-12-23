d044c3fa6d2f851eb271dc427a8b154a228bcb8e
Paco Lopez <francisco.lopsan@gmail.com>
2020-12-24 00:18:23 +0100

    Move SafetyChecker to components folder

--- src/components/SafetyChecker/SafetyChecker.js
+++ src/components/SafetyChecker/SafetyChecker.js
@@ -1,32 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
-import { GET_ME } from "./queries/user";
+import { GET_ME } from "../../queries/user";
 import { useQuery } from "@apollo/client";
-import { saveUserInfo } from "./actions/userInfo";
-import { userLogout } from "./actions/root";
-import { useTranslation } from "react-i18next";
-import { Spinner } from "react-bootstrap";
+import { saveUserInfo } from "../../actions/userInfo";
+import { userLogout } from "../../actions/root";
 
 const SafetyChecker = ({ user, saveUserInfo, children }) => {
-  const { t } = useTranslation();
-  const { loading, error, data } = useQuery(GET_ME, {
+  const { error, data } = useQuery(GET_ME, {
     fetchPolicy: "network-only",
   });
-
-  if (loading)
-    return (
-      <>
-        <h1>{t("home.welcome")}</h1>
-        <h3>{t("home.wateringTrees")}</h3>
-        <Spinner
-          as="span"
-          animation="border"
-          size="lg"
-          role="status"
-          aria-hidden="true"
-        />
-      </>
-    );
 
   if (error) userLogout();
 
